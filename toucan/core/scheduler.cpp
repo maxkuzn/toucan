49c1b102ab7a7b89e59fed7020a50f854c8170f8
Maxim Kuznetsov <m.a.kuzn@yandex.ru>
2019-09-04 21:47:45 +0300

    Fixed bug

--- toucan/core/scheduler.cpp
+++ toucan/core/scheduler.cpp
@@ -77,6 +77,7 @@ void Scheduler::Terminate() {
 void Scheduler::Destroy(Fiber* fiber) {
     delete fiber;
     if (--tasks_ == 0) {
+        std::unique_lock<twist::mutex> lock(wait_mutex_);
         wait_cv_.notify_all();
     }
 }
