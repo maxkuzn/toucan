95b575499c9fd3077a9b3f6408f7d5afd9367383
Maxim Kuznetsov <m.a.kuzn@yandex.ru>
2019-09-05 23:15:48 +0300

    Added full tests for mutex

--- tests/test_mutex.cpp
+++ tests/test_mutex.cpp
@@ -1,73 +1,149 @@
 #include "gtest/gtest.h"
 
 #include <toucan/core/scheduler.hpp>
-#include <toucan/algo/fifo.hpp>
 #include <toucan/core/api.hpp>
-
 #include <toucan/sync/mutex.hpp>
 
-#include <iostream>
+#include "support/fiber_barrier.hpp"
+#include "support/all_algos.hpp"
+
+#include <ctime>
 #include <chrono>
 
 using namespace toucan;
-using namespace algo;
+using namespace toucan::testing;
 
-TEST(Mutex, LockUnlock) {
-    Scheduler scheduler(std::make_shared<FIFO>(), 4);
-    toucan::Mutex mutex;
+// for 1s, 5ms and so on
+using namespace std::chrono_literals;
+
+/////////////////////////////////////////////////////////////////////
+
+#if defined(__clang__)
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+#endif
+
+/////////////////////////////////////////////////////////////////////
+
+// Set up different IAlgorithms in gtest
+
+template <typename Algo>
+class MutexTest : public ::testing::Test {
+  protected:
+    std::shared_ptr<algo::IAlgorithm> algo = std::make_shared<Algo>();
+};
+
+TYPED_TEST_SUITE(MutexTest, AllAlgorithmsTypes);
+
+/////////////////////////////////////////////////////////////////////
+
+// Tests
+
+TYPED_TEST(MutexTest, SimpleCounting) {
+    static const size_t kTasks = 100;
+    static const size_t kIncrements = 1000;
+
+    size_t count = 0;
+    FiberBarrier barrier(kTasks);
+    Mutex mutex;
+
+    auto task = [&] {
+        barrier.PassThrough();
+        for (size_t i = 0; i != kIncrements; ++i) {
+            mutex.Lock();
+            ++count;
+            mutex.Unlock();
+        }
+    };
+
+    Scheduler scheduler(this->algo, 4);
+    for (size_t i = 0; i != kTasks; ++i) {
+        scheduler.Spawn(task);
+    }
+    scheduler.WaitAll();
+
+    ASSERT_EQ(count, kTasks * kIncrements);
+}
+
+// Doesn't work now because of workers busy wait for fibers
+TYPED_TEST(MutexTest, DISABLED_NoBusyWait) {
+    FiberBarrier barrier(2);
+    Mutex mutex;
+
+    Scheduler scheduler(this->algo, 4);
     scheduler.Spawn([&] {
         mutex.Lock();
+        std::this_thread::sleep_for(1s);
         mutex.Unlock();
     });
+
+    std::this_thread::sleep_for(200ms);
+
+    auto start_clock = std::clock();
+
+    scheduler.Spawn([&] {
+        auto start = std::chrono::steady_clock::now();
+        mutex.Lock();
+        auto end = std::chrono::steady_clock::now();
+        ASSERT_GT(end - start, 500ms);
+        mutex.Unlock();
+    });
+
     scheduler.WaitAll();
+
+    auto cpu_time_seconds = static_cast<double>(std::clock() - start_clock) / CLOCKS_PER_SEC;
+    ASSERT_LT(cpu_time_seconds, 0.1);
 }
 
-TEST(Mutex, Explicit) {
-    Scheduler scheduler(std::make_shared<FIFO>(), 4);
-    static const size_t kTasks = 10;
-    static const size_t kIters = 10;
-    toucan::Mutex mutex;
-    static const auto kIdleTime = std::chrono::milliseconds(1);
-    std::atomic<size_t> count = 0;
-    auto task = [&] {
-        for (size_t i = 0; i != kIters; ++i) {
-            mutex.Lock();
-            std::this_thread::sleep_for(kIdleTime);
-            mutex.Unlock();
-        }
-        ++count;
-    };
-    auto start = std::chrono::system_clock::now();
-    for (size_t i = 0; i != kTasks; ++i) {
-        scheduler.Spawn(task);
-    }
-    scheduler.WaitAll();
-    auto end = std::chrono::system_clock::now();
-    ASSERT_EQ(count, kTasks);
-    ASSERT_GT(end - start, kTasks * kIters * kIdleTime);
-}
+TYPED_TEST(MutexTest, DontBlockThread) {
+    Mutex mutex;
 
-TEST(Mutex, Stress) {
-    Scheduler scheduler(std::make_shared<FIFO>(), 8);
-    std::atomic<size_t> count = 0;
-    static const size_t kTasks = 100;
-    static const size_t kCycles = 100;
-    toucan::Mutex mutex;
-    size_t sum = 0;
-    auto task = [&] {
-        for (size_t i = 0; i != kCycles; ++i) {
-            mutex.Lock();
+    auto holder = [&mutex]() {
+        mutex.Lock();
+
+        auto start = std::chrono::steady_clock::now();
+        auto end = start;
+        do {
             Yield();
-            ++sum;
-            mutex.Unlock();
-        }
-        ++count;
+            end = std::chrono::steady_clock::now();
+        } while (end - start < 1s);
+
+        mutex.Unlock();
     };
-    for (size_t i = 0; i != kTasks; ++i) {
-        scheduler.Spawn(task);
-    }
+
+    auto waiter = [&mutex]() {
+        mutex.Lock();
+        mutex.Unlock();
+    };
+
+    static const size_t kWorkerSteps = 10000;
+
+    std::atomic<size_t> worker_steps = 0;
+
+    auto worker = [&worker_steps]() {
+        for (size_t i = 0; i != kWorkerSteps; ++i) {
+            worker_steps.store(worker_steps.load() + 1);
+            Yield();
+        }
+    };
+
+    Scheduler scheduler(this->algo, 1);
+    scheduler.Spawn(holder);
+
+    std::this_thread::sleep_for(200ms);
+
+    scheduler.Spawn(waiter);
+    scheduler.Spawn(worker);
+
+    std::this_thread::sleep_for(500ms);
+    ASSERT_EQ(worker_steps, kWorkerSteps);
+
     scheduler.WaitAll();
-    ASSERT_EQ(count, kTasks);
-    ASSERT_EQ(sum, kTasks * kCycles);
 }
+
+/////////////////////////////////////////////////////////////////////
+
+#if defined(__clang__)
+#    pragma clang diagnostic pop
+#endif
 
